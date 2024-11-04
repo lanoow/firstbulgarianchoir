c451ff390bb7aba26a7c51dd528825d4048aff97
Borislav (lanoow) <me@lanoow.dev>
2024-11-04 13:37:47 +0200

    v0.6.3 (next 14)

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,5 +1,6 @@
-import { notFound } from "next/navigation";
+import { setRequestLocale } from "next-intl/server";
 import { getRequestConfig } from "next-intl/server";
+import { notFound } from "next/navigation";
 import { routing } from "./routing";
 import { Formats } from "next-intl";
 
@@ -27,6 +28,8 @@ export const formats = {
 export default getRequestConfig(async ({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!routing.locales.includes(locale as any)) notFound();
+
+  setRequestLocale(locale);
 
   return {
     messages: (await import(`../../messages/${locale}.json`)).default,
