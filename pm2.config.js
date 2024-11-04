18d943f97ec9c6a0c9c82443efa525e304281829
Borislav (lanoow) <me@lanoow.dev>
2024-11-04 10:32:29 +0200

    v0.6.3 (next 14)

--- pm2.config.js
+++ pm2.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: "fbc", // Name of your application
-  script: "index.ts", // Entry point of your application
-  // interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
+  // script: "index.ts", // Entry point of your application
+  interpreter: "~/.bun/bin/bun", // Path to the Bun interpreter
 };
