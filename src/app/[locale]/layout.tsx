09007febbbdd1cd5ebb464016cbf0dcd0832144d
Borislav (lanoow) <me@lanoow.dev>
2024-11-18 08:38:36 +0200

    v0.8.5

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,13 +4,21 @@ import { NextIntlClientProvider } from "next-intl";
 import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 
-// @ts-expect-error Builds and works just fine, vercel is just ass
-export default async function LocaleLayout(props: Readonly<{children: React.ReactNode;params: { locale: string };}>) {
+// @ts-nocheck
+export default async function LocaleLayout(
+	// @ts-nocheck
+	props: Readonly<{
+		// @ts-nocheck
+		children: React.ReactNode;
+		// @ts-nocheck
+		params: { locale: string };
+	}>
+) {
 	const { children, params: { locale } } = props;
 
 	if (!routing.locales.includes(locale as any)) {
-    notFound();
-  }
+		notFound();
+	}
 
 	setRequestLocale(locale);
 
